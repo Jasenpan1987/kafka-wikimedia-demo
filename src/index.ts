7097d546d833ea8c8225ad627f80bc10d98557f6
jasenpan <jasenpan@gmail.com>
2023-02-12 17:51:02 +1100

    basic kafka and wikimedia stream setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-import { getEnv } from "./utils/env";
+import WikimediaStream from "wikimedia-streams";
+import { Kafka, logLevel } from "kafkajs";
 
-function main() {
-  const author = getEnv("AUTHOR");
-  console.log("hello from " + author);
+const WIKIMEDIA_STREAM_NAME = "recentchange";
+const KAFKA_TOPIC = "wikimedia.recentchange.nodejs";
+
+async function main() {
+  const kafka = new Kafka({
+    logLevel: logLevel.DEBUG,
+    brokers: ["localhost:9092"],
+    clientId: "jasen-client"
+  });
+
+  const producer = kafka.producer({
+    allowAutoTopicCreation: true
+  });
+
+  await producer.connect();
+
+  const stream = new WikimediaStream([WIKIMEDIA_STREAM_NAME]);
+
+  stream.on("mediawiki.recentchange", async (data, evemt) => {
+    await producer.send({
+      topic: KAFKA_TOPIC,
+      messages: [{ value: JSON.stringify(data) }]
+    });
+  });
 }
 
 main();
